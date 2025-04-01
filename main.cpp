203f9a693d9c5f8b2092ad9e5daead8b89fd39ff
David Gillies <daggillies@gmail.com>
2025-03-31 21:49:29 -0600

    add namespace

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
 // g++ -std=c++23 main.cpp -o range_adaptors
 #include <iostream>
+#include <ranges>
+#include <vector>
 
 #include "range_adaptors.h"
 
 using namespace std::string_literals;
+using namespace RangeAdaptor;
+
+void dropit(const std::vector<int> &v, const struct drop_last_t &d) {
+  auto dv = v | d | std::ranges::to<std::vector<int>>();
+  for (auto &e : v) {
+    std::cout << e << " ";
+  }
+  std::cout << "\n";
+}
 
 int main() {
   auto s{"0123456789"s};
 
-  auto dln = s | drop_last_n(6) | std::ranges::to<std::string>();
+  // fully qualified with namespace
+  auto dln = s | RangeAdaptor::drop_last_n(6) | std::ranges::to<std::string>();
   std::cout << "|" << dln << "|\n";
   auto dl = s | drop_last | std::ranges::to<std::string>();
   std::cout << "|" << dl << "|\n";
+  auto dd = drop_last_t();
+  auto dldd = s | dd | std::ranges::to<std::string>();
+  std::cout << "|" << dldd << "|\n";
 
   auto sn{"0123456789"s};
 
@@ -19,6 +34,9 @@ int main() {
   std::cout << "|" << sndts << "|\n";
   sndts = sn | drop_two_sided(12, 3) | std::ranges::to<std::string>();
   std::cout << "|" << sndts << "|\n";
+
+  auto vi = std::vector<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  dropit(vi, drop_last);
 
   auto irng = std::views::iota(0) | std::views::take(20);
   for (int i : irng) {
--- range_adaptors.h
+++ range_adaptors.h
@@ -2,75 +2,76 @@
 #define __RANGE_ADAPTORS_H__
 #include <ranges>
 
-struct drop_last_n_t : std::ranges::range_adaptor_closure<drop_last_n_t> {
-  std::size_t len;
+namespace RangeAdaptor {
+  struct drop_last_n_t : std::ranges::range_adaptor_closure<drop_last_n_t> {
+    std::size_t len;
 
-  constexpr drop_last_n_t(std::size_t len) : len(len){};
+    constexpr drop_last_n_t(std::size_t len) : len(len){};
 
-  template <std::ranges::viewable_range R>
-  auto operator()(R&& r) const {
-    if constexpr (std::ranges::sized_range<R>) {
-      auto n = std::ranges::size(r);
-      return std::views::take(std::forward<R>(r), n > len ? n - len : 0);
-    } else {
-      auto head = std::ranges::begin(r);
-      auto tail = std::ranges::end(r);
-      auto n = std::ranges::distance(head, tail);
-      auto ntail = std::ranges::next(head, n > len ? n - len : 0, tail);
-      return std::ranges::subrange(std::move(head), std::move(ntail));
-    }
-  }
-};
-inline constexpr auto drop_last_n(std::size_t len) { return drop_last_n_t{len}; };
-
-struct drop_last_t : drop_last_n_t {
-  constexpr drop_last_t() : drop_last_n_t{1} {};
-};
-inline constexpr drop_last_t drop_last;
-
-struct drop_two_sided_t : std::ranges::range_adaptor_closure<drop_two_sided_t> {
-  std::size_t front, back;
-
-  constexpr drop_two_sided_t(std::size_t front, std::size_t back) : front(front), back(back) {}
-
-  template <std::ranges::viewable_range R>
-  constexpr auto operator()(R&& r) const {
-    if constexpr (std::ranges::sized_range<R>) {
-      auto n = std::ranges::size(r);
-      return (front + back >= n) ? std::views::drop(std::forward<R>(r), 0) | std::views::take(0)
-                                 : std::views::drop(std::forward<R>(r), front) | std::views::take(n - front - back);
-    } else {
-      auto first = std::ranges::begin(r);
-      auto last = std::ranges::end(r);
-
-      first = std::ranges::next(first, front, last);
-      auto newLast = std::ranges::next(first, std::ranges::distance(first, last) - back, last);
-      return std::ranges::subrange(std::move(first), std::move(newLast));
-    }
-  }
-};
-inline constexpr auto drop_two_sided(std::size_t front, std::size_t back) { return drop_two_sided_t{front, back}; }
-
-struct substr_t : std::ranges::range_adaptor_closure<substr_t> {
-  std::size_t start, len;
-
-  constexpr substr_t(std::size_t start, std::size_t len) : start(start), len(len) {}
-
-  template <std::ranges::viewable_range R>
-  constexpr auto operator()(R&& r) const {
-    if constexpr (std::ranges::sized_range<R>) {
-      auto n = std::ranges::size(r);
-      return (start >= n) ? std::views::drop(std::forward<R>(r), 0) | std::views::take(0)
-                          : std::views::drop(std::forward<R>(r), start) | std::views::take(len);
-    } else {
-      auto head = std::ranges::begin(r);
-      auto tail = std::ranges::end(r);
-      head = std::ranges::next(head, start, tail);
-      auto newTail = std::ranges::next(head, len, tail);
-      return std::ranges::subrange(std::move(head), std::move(newTail));
+    template <std::ranges::viewable_range R>
+    auto operator()(R&& r) const {
+      if constexpr (std::ranges::sized_range<R>) {
+        auto n = std::ranges::size(r);
+        return std::views::take(std::forward<R>(r), n > len ? n - len : 0);
+      } else {
+        auto head = std::ranges::begin(r);
+        auto tail = std::ranges::end(r);
+        auto n = std::ranges::distance(head, tail);
+        auto ntail = std::ranges::next(head, n > len ? n - len : 0, tail);
+        return std::ranges::subrange(std::move(head), std::move(ntail));
+      }
     }
   };
-};
-inline constexpr auto substr(std::size_t start, std::size_t len) { return substr_t{start, len}; }
+  inline constexpr auto drop_last_n(std::size_t len) { return drop_last_n_t{len}; };
 
+  struct drop_last_t : drop_last_n_t {
+    constexpr drop_last_t() : drop_last_n_t{1} {};
+  };
+  inline constexpr drop_last_t drop_last;
+
+  struct drop_two_sided_t : std::ranges::range_adaptor_closure<drop_two_sided_t> {
+    std::size_t front, back;
+
+    constexpr drop_two_sided_t(std::size_t front, std::size_t back) : front(front), back(back) {}
+
+    template <std::ranges::viewable_range R>
+    constexpr auto operator()(R&& r) const {
+      if constexpr (std::ranges::sized_range<R>) {
+        auto n = std::ranges::size(r);
+        return (front + back >= n) ? std::views::drop(std::forward<R>(r), 0) | std::views::take(0)
+                                   : std::views::drop(std::forward<R>(r), front) | std::views::take(n - front - back);
+      } else {
+        auto first = std::ranges::begin(r);
+        auto last = std::ranges::end(r);
+
+        first = std::ranges::next(first, front, last);
+        auto newLast = std::ranges::next(first, std::ranges::distance(first, last) - back, last);
+        return std::ranges::subrange(std::move(first), std::move(newLast));
+      }
+    }
+  };
+  inline constexpr auto drop_two_sided(std::size_t front, std::size_t back) { return drop_two_sided_t{front, back}; }
+
+  struct substr_t : std::ranges::range_adaptor_closure<substr_t> {
+    std::size_t start, len;
+
+    constexpr substr_t(std::size_t start, std::size_t len) : start(start), len(len) {}
+
+    template <std::ranges::viewable_range R>
+    constexpr auto operator()(R&& r) const {
+      if constexpr (std::ranges::sized_range<R>) {
+        auto n = std::ranges::size(r);
+        return (start >= n) ? std::views::drop(std::forward<R>(r), 0) | std::views::take(0)
+                            : std::views::drop(std::forward<R>(r), start) | std::views::take(len);
+      } else {
+        auto head = std::ranges::begin(r);
+        auto tail = std::ranges::end(r);
+        head = std::ranges::next(head, start, tail);
+        auto newTail = std::ranges::next(head, len, tail);
+        return std::ranges::subrange(std::move(head), std::move(newTail));
+      }
+    };
+  };
+  inline constexpr auto substr(std::size_t start, std::size_t len) { return substr_t{start, len}; }
+}  // namespace RangeAdaptor
 #endif
